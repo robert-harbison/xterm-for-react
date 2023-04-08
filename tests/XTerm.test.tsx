5aab3b53d6e7996a57dc874d320afc221604095a
Jaxton Winder <jjwinder9@gmail.com>
2023-04-08 02:13:26 -0600

    Mocked matchMedia function for the XTerm test due to jest updates/unimplemented jsdom methods

--- tests/XTerm.test.tsx
+++ tests/XTerm.test.tsx
@@ -2,6 +2,22 @@ import { mount } from 'enzyme'
 import * as React from 'react'
 import { XTerm } from '../src'
 
+// Mock 'matchMedia' function to make the test work with jsdom/jest
+//   https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
+Object.defineProperty(window, 'matchMedia', {
+	writable: true,
+	value: jest.fn().mockImplementation(query => ({
+	  matches: false,
+	  media: query,
+	  onchange: null,
+	  addListener: jest.fn(), // deprecated
+	  removeListener: jest.fn(), // deprecated
+	  addEventListener: jest.fn(),
+	  removeEventListener: jest.fn(),
+	  dispatchEvent: jest.fn(),
+	})),
+});
+
 describe('<XTerm />', () => {
 	it('should render correctly', () => {
 		const wrapper = mount(<XTerm />)
